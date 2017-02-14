9539e09998e8af7c90e1618126ddd46d475942f5
Maria <mkalikas@live.com>
2017-02-14 14:21:54 -0500

    added type.hpp

---
+++ type.hpp
@@ -0,0 +1,77 @@
+#ifndef TYPE_HPP
+#define TYPE_HPP
+
+#include "ast.hpp"
+
+#include <iostream>
+
+Type*
+check(Context& cxt, Expr* e)
+{
+  struct V : Expr::Visitor {
+    Context& cxt;
+    Type* r;
+    V(Context& c) : cxt(c) { }
+    void visit(Bool_expr* e) {
+      r = &cxt.bool_type;
+    }
+    void visit(And_expr* e) {
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.bool_type;
+    }
+    void visit(Or_expr* e) {
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.bool_type;
+    }
+    void visit(Not_expr* e) {
+      // check that e->e1 is bool
+      r = &cxt.bool_type;
+    }
+    void visit(Equal_expr* e){
+    r = &cxt.bool_type;
+    }
+    void visit(Not_Equal_expr* e){
+    r = &cxt.bool_type;
+    }
+    void visit(Greater_than_expr* e){
+    r = &cxt.bool_type;
+    }
+    void visit(Less_than_expr* e){
+    r = &cxt.bool_type;
+    }
+    void visit(Greater_orEqual_expr* e){
+    r = &cxt.bool_type;
+    }
+    void visit(Less_orEqual_expr* e){
+    r = &cxt.bool_type;
+    }
+    void visit(Add_expr* e){
+    r = &cxt.int_type;
+    }
+    void visit(Sub_expr* e){
+    r = &cxt.int_type;
+    }
+    void visit(Mult_expr* e){
+    r = &cxt.int_type;
+    }
+    void visit(Div_expr* e){
+    r = &cxt.int_type;
+    }
+    void visit(Rem_expr* e){
+    r = &cxt.int_type;
+    }
+    void visit(Neg_expr* e){
+    r = &cxt.int_type;}
+
+
+  };
+  V vis(cxt);
+  e->accept(vis);
+  return vis.r;
+}
+
+
+
+#endif
