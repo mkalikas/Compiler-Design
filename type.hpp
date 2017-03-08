f967163eedf0c0b91ccf0314fd65912cf26843f7
mkalikas <mkalikas@live.com>
2017-03-07 22:24:16 -0500

    added comments

--- type.hpp
+++ type.hpp
@@ -30,37 +30,59 @@ check(Context& cxt, Expr* e)
       r = &cxt.bool_type;
     }
     void visit(Equal_expr* e){
-    r = &cxt.bool_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.bool_type;
     }
     void visit(Not_Equal_expr* e){
-    r = &cxt.bool_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.bool_type;
     }
     void visit(Greater_than_expr* e){
-    r = &cxt.bool_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.bool_type;
     }
     void visit(Less_than_expr* e){
-    r = &cxt.bool_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.bool_type;
     }
     void visit(Greater_orEqual_expr* e){
-    r = &cxt.bool_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.bool_type;
     }
     void visit(Less_orEqual_expr* e){
-    r = &cxt.bool_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.bool_type;
     }
     void visit(Add_expr* e){
-    r = &cxt.int_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.int_type;
     }
     void visit(Sub_expr* e){
-    r = &cxt.int_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.int_type;
     }
     void visit(Mult_expr* e){
-    r = &cxt.int_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.int_type;
     }
     void visit(Div_expr* e){
-    r = &cxt.int_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.int_type;
     }
     void visit(Rem_expr* e){
-    r = &cxt.int_type;
+      // check that e->e1 is bool
+      // check that e->e2 is bool
+      r = &cxt.int_type;
     }
     void visit(Neg_expr* e){
     r = &cxt.int_type;}
