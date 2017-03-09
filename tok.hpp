134d9816465e14439c94c9a85d283be276aff74e
Maria <mkalikas@live.com>
2017-03-09 11:47:54 -0500

    doing homework.

--- tok.hpp
+++ tok.hpp
@@ -5,6 +5,7 @@
 
 struct Token{
 	int kind;
+	Token(int k) : kind(k) {} 
 };
 
 struct PuncOp_tok : Token{
@@ -20,8 +21,10 @@ enum tok_kind{
 	star_tok; // used for multiplication
 	slash_tok; // used for division
 	percent_tok; // modulus operator, used for remainder of division
-	ampand_tok; // used for and
-	pike_tok; // used for or
+	ampand_tok; // used for bitwise and
+	and_tok; // &&
+	pike_tok; // used for bitwise or
+	or_tok; // ||
 	equal_tok; 
 	exclamation_tok; // used for complement 
 	notequ_tok; // !=
@@ -39,11 +42,14 @@ enum tok_kind{
 struct Bool_tokens : Token{
 
 	bool val; // True or False
+	std::string str; 
+	Bool_tokens() // have to finish this
 
 };
 
 struct Int_tokens : Token{
 	int val; // includes the numbers 0 - 9 and hexadecimal literals A - F
+	Int_tokens(Token t, int v) : val(v) {} // have to add other parameter
 
 };
 
