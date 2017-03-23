94eeb86db773570832a184b834b8007dcaf7b629
Maria <mkalikas@live.com>
2017-03-23 14:15:07 -0400

    changed constructors in Int_tokens class

--- tok.hpp
+++ tok.hpp
@@ -4,51 +4,8 @@
 #define TOK_HPP
 #include <string>
 
-struct Token{
-	int kind;
-	
-	
-	// 	Enumerated punctuation and operation tokens
-	enum tok_kind{
-		lparen_tok,
-		rparen_tok,
-		plus_tok,
-		minus_tok,
-		star_tok, // used for multiplication
-		slash_tok, // used for division
-		percent_tok, // modulus operator, used for remainder of division
-		ampand_tok, // used for bitwise and
-		and_tok, // &&
-		pike_tok, // used for bitwise or
-		or_tok, // ||
-		equal_tok,
-		exclamation_tok, // used for complement
-		notequ_tok, // !=
-		lt_tok, // less than symbol
-		lt_eq_tok, // less than or equal
-		gt_tok, // greater than symbol
-		gt_eq_tok, // greater than or eequal
-		question_tok, // question mark
-		colon_tok,
-		int_tok,
-		bool_tok,
-		pound_tok // # used for comments
-
-	};
-	//tok_kind kind;
-	Token();
-	Token(tok_kind k);
-	~Token();
-};
-
-struct PuncOp_tok: Token{
-	// tok_kind is represented by symbol
-	//Token t;
-	//PuncOp_tok(Token::tok_kind);
-};
-/*
 // 	Enumerated punctuation and operation tokens
-enum class tok_kind{
+enum tok_kind{
 	lparen_tok,
 	rparen_tok,
 	plus_tok,
@@ -71,35 +28,29 @@ enum class tok_kind{
 	colon_tok,
 	int_tok,
 	bool_tok,
-	pound_tok // # used for comments
-
+	eof_tok
 };
-*/
-
-
+struct Token{
+	tok_kind kind;
+	Token(tok_kind k) : kind(k) { }
+	virtual ~Token();
+};
 
 struct Bool_tokens: Token {
 
 	bool val; // True or False
-	std::string str; // need to finish this 
+	Bool_tokens(bool b) : Token(bool_tok), val(b) { }
 
 };
 
 struct Int_tokens: Token {
 	int val; // includes the numbers 0 - 9 and hexadecimal literals A - F
-	Int_tokens(tok_kind t, int v) { 
-		t = tok_kind::int_tok; 
-		v = val; 
-	} 
+	int radix; // used to denote hexadecimal and binary numbers
+	Int_tokens(tok_kind t, int v) : Token(int_tok), val(v) { }
+	Int_tokens(tok_kind t, int v, int r) : Token(int_tok), val(v), radix(r) { }
+	
 };
 
-struct Pound_tok: Token {
-	std::string str;
-	Pound_tok(tok_kind p, std::string s){
-		p = tok_kind::pound_tok;
-		s = str;
-	}
-};
 
 
 
