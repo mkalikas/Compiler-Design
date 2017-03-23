02db6b4b19a4a08b0c9c2a447ad43f79bd81a727
Maria <mkalikas@live.com>
2017-03-23 14:14:31 -0400

    fixed cases to return correct token

--- lexer.cpp
+++ lexer.cpp
@@ -2,6 +2,7 @@
 #include "lexer.hpp"
 
 #include <iostream>
+#include <exception>
 
 
 // Definition of consume() function
@@ -9,11 +10,8 @@
 char Lexer::consume(){
 	if(eof())
 		return 0;
-	//char c = s.get();
-	//buf += c;
 	buf += *first++;
 	++column;
-	//return c;
 	return buf.back();
 }
 
@@ -21,30 +19,28 @@ char Lexer::consume(){
 Token Lexer::next() {
 	buf.clear();
 	ignore();
-	//Token t;
 	
     switch(LookAhead()) {
         case '+' :
             consume();
-           // t.kind = Token::plus_tok;
-            return new Token(Token::plus_tok);
+            return Token(plus_tok);
         case '-' :
             consume();
-            return new PuncOp_tok(minus_tok);
+            return Token(minus_tok);
         case '*' :
             consume();
-            return new PuncOp_tok(star_tok);
+            return Token(star_tok);
         case '/' :
             consume();
-            return new PuncOp_tok(slash_tok);
+            return Token(slash_tok);
         case '%' :
             consume();
-            return new PuncOp_tok(percent_tok);
+            return Token(percent_tok);
         case '&' :
             consume();
             if(LookAhead() == '&'){
                 consume();
-                return new PuncOp_tok(ampand_tok);
+                return Token(ampand_tok);
             }
             else
                 // abort program
@@ -53,11 +49,11 @@ Token Lexer::next() {
             consume();
             if(LookAhead() == '|'){
                 consume();
-                return new PuncOp_tok(or_tok);
+                return Token(or_tok);
             }
-            else if((LookAhead() == std::isdigit(LookAhead())) || ((LookAhead()) == std::isalpha(LookAhead())){
+            else if((LookAhead() == std::isdigit(LookAhead())) || ((LookAhead()) == std::isalpha(LookAhead()))){
                 consume();;
-                return new PuncOp_tok(pike_tok);
+                return Token(pike_tok);
             }
 
             else
@@ -67,18 +63,18 @@ Token Lexer::next() {
             consume();
             if(LookAhead() == '='){
                 consume();
-                return new PuncOp_tok(notequ_tok);
+                return Token(notequ_tok);
             }
             else if(LookAhead() == std::isdigit(LookAhead())){ // complement with integer values
-                consume();
-                return new int_tok(int_tok, val);
+                char c = consume();
+                return  Int_tokens(int_tok, c);
             }
             // else if(LookAhead() == complement for boolean literal
         case '=' :
             consume();
             if(LookAhead() == '='){
                 consume();
-                return new PuncOp_tok(equal_tok);
+                return Token(equal_tok);
             }
             else
                 abort(); // invalid input
@@ -86,116 +82,54 @@ Token Lexer::next() {
             consume();
             if(LookAhead() == '='){
                 consume();
-                return new PuncOp(lt_eq_tok);
+                return Token(lt_eq_tok);
             }
             else{
                 consume();
-                return new PuncOp_tok(lt_tok);
+                return Token(lt_tok);
             }
         case '>' :
             consume();
             if(LookAhead() == '='){
                 consume();
-                return new PuncOp_tok(gt_eq_tok);
+                return Token(gt_eq_tok);
             }
             else{
                 consume();
-                return new PuncOp_tok(gt_tok);
+                return Token(gt_tok);
             }
         case '?' :
             consume();
-            return new PuncOp_tok(question_tok);
+            return Token(question_tok);
         case ':' :
             consume();
-            return new PuncOp_tok(colon_tok);
+            return Token(colon_tok);
         case '(' :
             consume();
-            return new PuncOp_tok(lparen_tok);
+            return Token(lparen_tok);
         case ')' :
             consume();
-            return new PuncOp_tok(rparen_tok);
+            return Token(rparen_tok);
         case '0' : case '1' : case '2' : case '3' : case '4' :
         case '5' : case '6' : case '7' : case '8' : case '9' : 
         case 'A' : case 'a' : case 'B' : case 'b' : case 'C' : 
         case 'c' : case 'D' : case 'd' : case 'E' : case 'e' :
         case 'F' : case 'f' : {
-            const char *Iter = first;
             consume();
-            while(!eof() && std::isxdigit(LookAhead()){
+            while(!eof() && std::isxdigit(LookAhead())){
                 consume();
-                std::string str(Iter, buf);
             }
-            int n = stoi(str);
-            return new Int_tokens(Int_tokens, n);
+            int n = stoi(buf);
+            return Int_tokens(int_tok, n);
         }
-        /*
-        case '0' :
-            consume();
-            return new Int_tokens(Int_tokens, 0);
-        case '1' :
-            consume()
-            return new Int_tokens(Int_tokens, 1);
-        case '2' :
-            consume();
-            return new Int_tokens(Int_tokens, 2);
-        case '3' :
-            consume();
-            return new Int_tokens(Int_tokens, 3);
-        case '4' :
-            consume();
-            return new Int_tokens(Int_tokens, 4);
-        case '5' :
-            consume():
-            return new Int_tokens(Int_tokens, 5);
-        case '6' :
-            consume();
-            return new Int_tokens(Int_tokens, 6);
-        case '7' :
-            consume();
-            return new Int_tokens(Int_tokens, 7);
-        case '8' :
-            consume():
-            return new Int_tokens(Int_tokens, 8);
-        case '9' :
-            consume();
-            return new Int_tokens(Int_tokens, 9);
-        
-        case 'A' :  //The following cases that include the letters from A to F represent hexadecimal literals
-            consume();
-            return new Int_tokens(Int_tokens, 10);
-        case 'B' :
-            consume();
-            return new Int_tokens(Int_tokens, 11);
-        case 'C' :
-            consume();
-            return new Int_tokens(Int_tokens, 12);
-        case 'D' :
-            consume();
-            return new Int_tokens(Int_tokens, 13);
-        case 'E' :
-            consume();
-            return new Int_tokens(Int_tokens, 14);
-        case 'F' :
-            consume();
-            return new Int_tokens(Int_tokens, 15);
-        
-        //case for binary integer literals to be included here
-        */
-        case '#' : {
-			const char *Iter = first;
-            consume();
-            while((LookAhead() != '\n') && (std::isalpha(LookAhead()) || std::isspace(LookAhead()))){
-                consume();
-                // add characters to an array
-				std::string str(Iter, buf);
-			}
-			return new Pound_tok(pound_tok, str);		 	
-		}
-            
+
+        case '#' :  // make this case recognize the symbol and delete entire comment line
+					 	
+
         default:
-            break;
-                  
+            break;         
     }
+    return Token(eof_tok);
      
 }
 
@@ -220,9 +154,4 @@ while(!eof()){
 
 }
 
-bool Lexer::match(char ch) {
-	if(LookAhead == k)
-		consume();
-	else 
-		throw syntax_error();
-	
+
