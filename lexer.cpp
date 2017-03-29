58b04c6a5bfa1b46478ebd21ec8c199a1863c896
Maria <mkalikas@live.com>
2017-03-29 02:44:11 -0400

    modified integer case

--- lexer.cpp
+++ lexer.cpp
@@ -19,138 +19,134 @@ char Lexer::consume(){
 Token Lexer::next() {
 	buf.clear();
 	ignore();
-	
-    switch(LookAhead()) {
-        case '+' :
-            consume();
-            return Token(plus_tok);
-        case '-' :
-            consume();
-            return Token(minus_tok);
-        case '*' :
-            consume();
-            return Token(star_tok);
-        case '/' :
-            consume();
-            return Token(slash_tok);
-        case '%' :
-            consume();
-            return Token(percent_tok);
-        case '&' :
-            consume();
-            if(LookAhead() == '&'){
-                consume();
-                return Token(ampand_tok);
-            }
-            else
-                // abort program
-                abort();
-        case '|' :
-            consume();
-            if(LookAhead() == '|'){
-                consume();
-                return Token(or_tok);
-            }
-            else if((LookAhead() == std::isdigit(LookAhead())) || ((LookAhead()) == std::isalpha(LookAhead()))){
-                consume();;
-                return Token(pike_tok);
-            }
 
-            else
-                // abort program
-                abort();
-        case '!' :
-            consume();
-            if(LookAhead() == '='){
-                consume();
-                return Token(notequ_tok);
-            }
-            else if(LookAhead() == std::isdigit(LookAhead())){ // complement with integer values
-                char c = consume();
-                return  Int_tokens(int_tok, c);
-            }
-            // else if(LookAhead() == complement for boolean literal
-        case '=' :
-            consume();
-            if(LookAhead() == '='){
-                consume();
-                return Token(equal_tok);
-            }
-            else
-                abort(); // invalid input
-        case '<' :
-            consume();
-            if(LookAhead() == '='){
-                consume();
-                return Token(lt_eq_tok);
-            }
-            else{
-                consume();
-                return Token(lt_tok);
-            }
-        case '>' :
-            consume();
-            if(LookAhead() == '='){
-                consume();
-                return Token(gt_eq_tok);
-            }
-            else{
-                consume();
-                return Token(gt_tok);
-            }
-        case '?' :
-            consume();
-            return Token(question_tok);
-        case ':' :
-            consume();
-            return Token(colon_tok);
-        case '(' :
-            consume();
-            return Token(lparen_tok);
-        case ')' :
-            consume();
-            return Token(rparen_tok);
-        case '0' : case '1' : case '2' : case '3' : case '4' :
-        case '5' : case '6' : case '7' : case '8' : case '9' : 
-        case 'A' : case 'a' : case 'B' : case 'b' : case 'C' : 
-        case 'c' : case 'D' : case 'd' : case 'E' : case 'e' :
-        case 'F' : case 'f' : {
-            consume();
-            while(!eof() && std::isxdigit(LookAhead())){
-                consume();
-            }
-            int n = stoi(buf);
-            return Int_tokens(int_tok, n);
-        }
+	switch(lookahead()) {
+		case '+' :
+			consume();
+			return Token(plus_tok);
+		case '-' :
+			consume();
+			return Token(minus_tok);
+		case '*' :
+			consume();
+			return Token(star_tok);
+		case '/' :
+			consume();
+			return Token(slash_tok);
+		case '%' :
+			consume();
+			return Token(percent_tok);
+		case '&' :
+			consume();
+			if(lookahead() == '&'){
+					consume();
+					return Token(ampand_tok);
+			}
+			else
+				// abort program
+				abort();
+		case '|' :
+			consume();
+		if(lookahead() == '|'){
+			consume();
+			return Token(or_tok);
+		}
+		else if((lookahead() == std::isdigit(lookahead())) || ((lookahead()) == std::isalpha(lookahead()))){
+			consume();;
+			return Token(pike_tok);
+		}
+		else
+			// abort program
+			abort();
+	case '!' :
+		consume();
+		if(lookahead() == '='){
+			consume();
+			return Token(notequ_tok);
+		}
+		else if(lookahead() == std::isdigit(lookahead())){ // complement with integer values
+			char c = consume();
+			return  Int_tokens(int_tok, c);
+		}
+		// else if(lookahead() == complement for boolean literal
+	case '=' :
+		consume();
+		if(lookahead() == '='){
+			consume();
+			return Token(equal_tok);
+		}
+		else
+				abort(); // invalid input
+	case '<' :
+		consume();
+		if(lookahead() == '='){
+			consume();
+			return Token(lt_eq_tok);
+		}
+		else{
+			consume();
+			return Token(lt_tok);
+		}
+	case '>' :
+		consume();
+		if(lookahead() == '='){
+			consume();
+			return Token(gt_eq_tok);
+		}
+		else{
+			consume();
+			return Token(gt_tok);
+		}
+	case '?' :
+		consume();
+		return Token(question_tok);
+	case ':' :
+		consume();
+		return Token(colon_tok);
+	case '(' :
+			consume();
+			return Token(lparen_tok);
+	case ')' :
+		consume();
+		return Token(rparen_tok);
+	case '0' : case '1' : case '2' : case '3' : case '4' :
+	case '5' : case '6' : case '7' : case '8' : case '9' : {
+		consume();
+		while(!eof() && std::isxdigit(lookahead())){
+			consume();
+		}
+		int n = stoi(buf);
+		return Int_tokens(int_tok, n);
+	}
 
-        case '#' :  // make this case recognize the symbol and delete entire comment line
-					 	
+	case '#' :  // make this case recognize the symbol and delete entire comment line
 
-        default:
-            break;         
-    }
-    return Token(eof_tok);
-     
+
+	default:
+		break;
+}
+	return Token(eof_tok);
+
 }
 
 void Lexer::skipSpace(){
-while(!eof()){
-	switch(LookAhead()){
-		case ' ' :
-			consume();
-			break;
-		case '\n' :
-			++line; 
-			column = 1; // moves to first input character on next line
-			break;
-		case '\t' : {
-			ignore();
-			continue;
-		default: 
-			return;
+	while(!eof()){
+		switch(lookahead()){
+			case ' ' :
+				consume();
+				break;
+			case '\n' :
+				++line;
+				column = 1; // moves to first input character on next line
+				break;
+			case '\t' : {
+				ignore();
+				continue;
+			default:
+				return;
+			}
 		}
 	}
-}
 
 }
 
