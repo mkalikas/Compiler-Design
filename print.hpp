4745a5048eb10e6453ae87771e9fc2b13c07c5f2
Maria <mkalikas@live.com>
2017-03-29 02:30:57 -0400

    modified file for conditional expressions

--- print.hpp
+++ print.hpp
@@ -11,9 +11,11 @@ needs_parens(Expr* e)
   struct V : Expr::Visitor {
     bool r;
     void visit(Bool_expr* e) { r = false; }
+    void visit(Int_expr* e) { r = false; }
     void visit(And_expr* e) { r = true; }
     void visit(Or_expr* e) { r = true; }
     void visit(Not_expr* e) { r = true; }
+    void visit(Cond_expr* e) { r = false; }
     void visit(Equal_expr* e) { r = true; }
     void visit(Not_Equal_expr* e) { r = true; }
     void visit(Less_than_expr* e) { r = true; }
@@ -53,6 +55,7 @@ print(Expr* e)
       else
         std::cout << "false";
     }
+
     void visit(And_expr* e) {
       print_enclosed(e->e1);
       std::cout << " & ";
@@ -67,6 +70,13 @@ print(Expr* e)
       std::cout << '!';
       print_enclosed(e->e1);
     }
+    void visit(Cond_expr* e) {
+			print_enclosed(e->e1);
+			std::cout << " ? ";
+			print_enclosed(e->e2);
+			std::cout << " : ";
+			print_enclosed(e->e3);
+		}
     void visit(Equal_expr* e) {
       print_enclosed(e->e1);
       std::cout << "==";
