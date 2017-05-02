9efd6d3208de2a5d2687a635f75ad33389ac882c
Maria <mkalikas@live.com>
2017-05-02 16:42:36 -0400

    adding semantics files for exprs and stmts

---
+++ semantics_expr.cpp
@@ -0,0 +1,66 @@
+#include "semantics.hpp"
+
+// Semantics of expressions
+Expr* Semantics::on_int(Token *) {
+
+}
+Expr* Semantics::on_bool(Token *) {
+
+}
+Expr* Semantics::on_id(Token *) {
+
+}
+Expr* Semantics::on_add(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_sub(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_mult(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_div(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_rem(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_negation(Expr* e1) {
+
+}
+Expr* Semantics::on_and(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_or(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_exclamation(Expr* e1) {
+
+}
+Expr* Semantics::on_conditional(Expr* e1, Expr* e2, Expr* e3) {
+
+}
+Expr* Semantics::on_assignment(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_equal(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_not_eq(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_lt(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_gt(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_lt_eq(Expr* e1, Expr* e2) {
+
+}
+Expr* Semantics::on_gt_eq(Expr* e1, Expr* e2) {
+
+}
+std::string Semantics::on_id(Token *id) {
+
+}
---
+++ semantics_stmt.cpp
@@ -0,0 +1,37 @@
+#include "semantics.hpp"
+
+// Semantics of statements
+
+Stmt* Semantics::on_block_stmt(Stmt *s) {
+
+}
+Stmt* Semantics::on_stmt_seq(Stmt *s) {
+
+}
+Stmt* Semantics::on_if_stmt(Expr *e, Stmt* s1, Stmt* s2) {
+
+}
+Stmt* Semantics::on_while_stmt(Expr *e, Stmt *s) {
+
+}
+Stmt* Semantics::on_break_stmt() {
+
+}
+Stmt* Semantics::on_contintue_stmt() {
+
+}
+Stmt* Semantics::on_return_stmt() {
+
+}
+Stmt* Semantics::on_assert_stmt() {
+
+}
+Stmt* Semantics::on_expr_stmt(Expr *e) {
+
+}
+Stmt* Semantics::on_decl_stmt(Decl *d) {
+
+}
+Stmt* Semantics::on_var_decl(Type t, Expr *e) {
+
+}
