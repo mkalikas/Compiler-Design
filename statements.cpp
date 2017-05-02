a4656be1b62308f4d0c59cde8f9bd6adc06af9d3
Maria <mkalikas@live.com>
2017-05-02 00:50:53 -0400

    modified

--- statements.cpp
+++ statements.cpp
@@ -1,53 +1,6 @@
 // Statements
 #include "parser.hpp"
 
-
-struct Stmt {
-
-
-};
-
-struct Block_stmt : Stmt {
-
-};
-
-struct If_Else_stmt : Stmt {
-
-};
-
-struct While_stmt : Stmt {
-
-};
-
-struct Break_stmt :Stmt {
-	
-};
-
-
-struct Continue_stmt : Stmt {
-	
-};
-
-struct Return_stmt : Stmt {
-	
-};
-
-struct Assert_stmt : Stmt
-{
-	
-};
-
-struct Expr_stmt : Stmt
-{
-	
-};
-
-struct Decl_stmt : Stmt
-{
-	Var_decl();
-};
-
-
 // definition of stmt member function
 Stmt Parser::stmt() {
 
